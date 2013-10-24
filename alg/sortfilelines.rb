188ad1515236eaf23aec71504e64a19a716ea5ba
Irene <irene@aurametrix.com>
2013-10-23 20:50:40 -0700

    sorted file in out.txt

--- alg/sortfilelines.rb
+++ alg/sortfilelines.rb
@@ -1,18 +1,22 @@
 ARGF.lines do |line|
+File.open("out.txt", 'w') { |file| file.write(ARGF.each_line.sort.join) } 
   puts ARGF.filename if ARGF.lineno == 1
   puts "#{ARGF.lineno}: #{line}"
 end
 
-puts "after sorting:"
+#puts "out.txt has content after sorting:"
 
 # this would work in 1.9.3 
 #File.write(ARGV.pop, ARGF.each_line.sort.join)
 
 # for ruby 1.9.2
-# copy_file(#{ARGF.filename}, "output.txt", preserve = false, dereference = true)
+# copy_file(ARGF, "output.txt", preserve = false, dereference = true)
 
-#new_array = File.readlines("in.txt").sort
-new_array = ARGF.each_line.sort.join
-puts new_array
+#File.open("out.txt", 'w') { |file| file.write(ARGF.each_line.sort.join) } 
 
-File.open('out.txt', 'w') {|f| f.write(new_array) }
+
+#new_array = File.readlines(#{ARGF}).sort
+#new_array = ARGF.each_line.sort.join
+#puts new_array
+
+#File.open('out.txt', 'w') {|f| f.write(new_array) }
